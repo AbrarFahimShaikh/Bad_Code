af65b860c46cf1dfed09ea8cb5bcbf610898179d
Abrar Fahim Shaikh <74659752+AbrarFahimShaikh@users.noreply.github.com>
2023-09-11 16:33:14 +0600

    Add files via upload

---
+++ Python/pdfmerge.py
@@ -0,0 +1,23 @@
+from PyPDF2 import PdfMerger
+
+# User input
+pdf1 = input(r"Enter path of first file: ")
+pdf2 = input(r"Enter path of second file: ")
+outputFile = input(r"Enter path for the output merged file: ")
+
+# Create object
+merger = PdfMerger()
+
+# Append the input PDF files one by one
+merger.append(pdf1)
+merger.append(pdf2)
+
+# Write the merged PDF to the output path
+merger.write(outputFile)
+
+# Close the merger
+merger.close()
+
+print(f"Merged PDF saved to {outputFile}")
+
+
